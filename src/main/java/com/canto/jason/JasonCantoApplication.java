258bdfc867f4e82cd0560166e4e436e71e4dc9c9
jason.canto <rofsdg@gmail.com>
2017-09-20 21:44:27 -0300

    First commit

---
+++ src/main/java/com/canto/jason/JasonCantoApplication.java
@@ -0,0 +1,12 @@
+package com.canto.jason;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasonCantoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasonCantoApplication.class, args);
+	}
+}
